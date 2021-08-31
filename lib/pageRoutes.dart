d891f7613f7fa8140a93397154d5fd820c247562
hasanshifat <shifatmad99@gmail.com>
2021-08-31 18:02:28 +0600

    route added

---
+++ lib/pageRoutes.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:tutor_finder/screens/teacher_ui/registration_page.dart';
+
+final Map<String, WidgetBuilder> routes = {
+  TeacherSignRegPage.routeName: (context) => TeacherSignRegPage(),
+};
