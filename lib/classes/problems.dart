0a25809e1b24d4e37533522e0ccb24920579b1ad
hasanshifat <shifatmad99@gmail.com>
2021-09-14 15:49:51 +0600

    prblm post edit

--- lib/classes/problems.dart
+++ lib/classes/problems.dart
@@ -1,5 +1,5 @@
 class ProblemsClass {
   String name, uid, qstn, propic;
-  var timestamp;
-  ProblemsClass({this.name, this.qstn, this.timestamp, this.uid, this.propic});
+  var time,date;
+  ProblemsClass({this.name, this.qstn, this.time,this.date, this.uid, this.propic});
 }
--- lib/screens/problems_page.dart
+++ lib/screens/problems_page.dart
@@ -11,6 +11,7 @@ import 'package:tutor_finder/components/colors.dart';
 import 'package:tutor_finder/components/cons_height_width.dart';
 import 'package:tutor_finder/components/mytext.dart';
 import 'package:intl/intl.dart';
+import 'package:tutor_finder/components/mytext2.dart';
 import 'package:tutor_finder/components/mytext_monserrat.dart';
 
 class Problems extends StatefulWidget {
@@ -42,16 +43,17 @@ class _ProblemsState extends State<Problems> {
       querySnapshot.docs.forEach((doc) {
         setState(() {
           Timestamp timestamp;
-          timestamp = doc['posted_time'];
-          var d12 = DateFormat('MM/dd/yyyy, hh:mm a')
-              .format(timestamp.toDate().toLocal());
+          timestamp = doc['posted_time']; //'MM/dd/yyyy, hh:mm a'
+          var date = DateFormat.yMMMMd().format(timestamp.toDate().toLocal());
+          var d12 = DateFormat.jm().format(timestamp.toDate());
+          var dateTime = date + d12;
           firestoreInstance.doc(doc['author_uid']).snapshots().listen((event) {
-            print(event.data()['name']);
             setState(() {
               problemsList.add(ProblemsClass(
                 qstn: doc['question'],
                 uid: doc['author_uid'],
-                timestamp: d12,
+                time: d12,
+                date: date,
                 name: event.data()['name'],
                 propic: event.data()['profile_pic'],
               ));
@@ -70,131 +72,142 @@ class _ProblemsState extends State<Problems> {
     return Scaffold(
       appBar: appBar(context, 'Problems'),
       body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5),
-        child: problemsList.length < 1
-            ? Center(
-                child: CircularProgressIndicator(),
-              )
-            : Container(
-                height: size.height * 1,
-                width: size.width * 1,
-                child: ListView.builder(
-                  physics: BouncingScrollPhysics(),
-                  itemCount: problemsList.length,
-                  itemBuilder: (context, i) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 5),
-                      child: Container(
-                        decoration: BoxDecoration(
-                          color: colorwhite,
-                          borderRadius: BorderRadius.circular(8),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black87.withOpacity(0.5),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 5, horizontal: 5),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.start,
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  CircleAvatar(
-                                    maxRadius: 25,
-                                    child: ClipOval(
-                                      child: problemsList[i].propic == 'N/A'
-                                          ? SvgPicture.asset(
-                                              'assets/images/boy.svg')
-                                          : Image.memory(
-                                              base64Decode(
-                                                  problemsList[i].propic),
-                                              width: 60,
-                                              height: 60,
-                                              fit: BoxFit.cover,
-                                            ),
+          child: problemsList.length < 1
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : Container(
+                  height: size.height * 1,
+                  width: size.width * 1,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: problemsList.length,
+                    itemBuilder: (context, i) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 5),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: colorwhite,
+                            borderRadius: BorderRadius.circular(8),
+                            // boxShadow: [
+                            //   BoxShadow(
+                            //     color: Colors.black87.withOpacity(0.5),
+                            //   ),
+                            // ],
+                          ),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 10, horizontal: 10),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    CircleAvatar(
+                                      maxRadius: 25,
+                                      child: ClipOval(
+                                        child: problemsList[i].propic == 'N/A'
+                                            ? SvgPicture.asset(
+                                                'assets/images/boy.svg')
+                                            : Image.memory(
+                                                base64Decode(
+                                                    problemsList[i].propic),
+                                                width: 60,
+                                                height: 60,
+                                                fit: BoxFit.cover,
+                                              ),
+                                      ),
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: 10,
-                                  ),
-                                  Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      MytextMontserrat(
-                                        text: problemsList[i].name,
-                                        fontsize: 15,
-                                        fontWeight: FontWeight.w500,
-                                      ),
-                                      Mytext(
-                                        text: problemsList[i].timestamp,
-                                        fontsize: 10,
-                                      ),
-                                    ],
-                                  ),
-                                ],
+                                    SizedBox(
+                                      width: 10,
+                                    ),
+                                    Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        MytextMontserrat(
+                                          text: problemsList[i].name,
+                                          fontsize: 15,
+                                          fontWeight: FontWeight.w500,
+                                        ),
+                                        Mytext(
+                                          text:
+                                              '${problemsList[i].date} at ${problemsList[i].time}',
+                                          fontsize: 10,
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 5),
-                              child: Mytext(
-                                text: problemsList[i].qstn,
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 5),
+                                child: Mytext(
+                                  text: problemsList[i].qstn,
+                                ),
                               ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 10, vertical: 10),
-                              child: Row(
-                                children: [
-                                  FaIcon(
-                                    FontAwesomeIcons.check,
-                                    size: 18,
-                                    color: loginbtn1,
-                                  ),
-                                  sw10,
-                                  Mytext(
-                                    text: '102',
-                                  ),
-                                  SizedBox(
-                                    width: 20,
-                                  ),
-                                  FaIcon(
-                                    FontAwesomeIcons.times,
-                                    size: 18,
-                                    color: Colors.red,
-                                  ),
-                                  sw10,
-                                  Mytext(
-                                    text: '12',
-                                  ),
-                                  SizedBox(
-                                    width: 50,
-                                  ),
-                                  FaIcon(
-                                    FontAwesomeIcons.comment,
-                                    size: 20,
-                                  ),
-                                  sw10,
-                                  Mytext(
-                                    text: '102',
-                                  ),
-                                ],
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(vertical: 10),
+                                child: Container(
+                                  width: size.width * 1,
+                                  height: 0.5,
+                                  color: colorgreylite.withOpacity(1),
+                                ),
                               ),
-                            )
-                          ],
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 10),
+                                child: Row(
+                                  children: [
+                                    sw10,
+                                    FaIcon(
+                                      FontAwesomeIcons.check,
+                                      size: 18,
+                                      color: loginbtn1,
+                                    ),
+                                    sw10,
+                                    Mytext2(
+                                      text: '102',
+                                    ),
+                                    Spacer(),
+                                    FaIcon(
+                                      FontAwesomeIcons.times,
+                                      size: 18,
+                                      color: Colors.red,
+                                    ),
+                                    sw10,
+                                    Mytext2(
+                                      text: '12',
+                                    ),
+                                    Spacer(),
+                                    FaIcon(
+                                      FontAwesomeIcons.commentDots,
+                                      size: 20,
+                                    ),
+                                    sw10,
+                                    Mytext2(
+                                      text: '102',
+                                    ),
+                                    sw10,
+                                  ],
+                                ),
+                              ),
+                              s10,
+                              Container(
+                                width: size.width * 1,
+                                height: 2,
+                                color: colorgreylite.withOpacity(1),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    );
-                  },
-                ),
-              ),
-      )),
+                      );
+                    },
+                  ),
+                )),
     );
   }
 }
